87e381298a77721d86988ec65900693489a5847f
Artem Kryvenko <artem.v.kryvenko@gmail.com>
2013-12-15 19:24:47 +0200

    configured posts

--- globetrotting/apps/blog/models.py
+++ globetrotting/apps/blog/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.core.urlresolvers import reverse
 
-# Create your models here.
+
+class Post(models.Model):
+	title = models.CharField(max_length=255)
+	slug = models.SlugField(unique=True, max_length=255)
+	description = models.CharField(max_length=255)
+	content = models.TextField()
+	created = models.DateField(auto_now_add=True)
+	published = models.BooleanField(default=True)
+
+	class Meta:
+		ordering = ['-created']
+
+	def __unicode__(self):
+		return "%s" % self.title
+
+	def get_absolute_url(self):
+		return reverse('blog.views.post', args=[self.slug])
--- globetrotting/apps/blog/views.py
+++ globetrotting/apps/blog/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from blog.models import Post
 
-# Create your views here.
+
+def index(request):
+	posts = Post.objects.filter(published=True)
+	data = {'posts': posts}
+	return render(request, 'blog/index.html', data)
+
+
+def post(request, slug):
+	post = get_object_or_404(Post, slug=slug)
+	data = {'post': post}
+	return render(request, 'blog/post.html', data)
